b65ba6cd363a77aa80308c947b996c76e8ee74d3
Joel Crasta <loejstarc@gmail.com>
2024-11-04 20:08:48 +0530

    cloudfront domain added

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "animoox-media-bucket.s3.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "daqnioq8w8j1r.cloudfront.net",
+      },
     ],
   },
 }
