7fff11f2d768600b90dca3824914601d68cd930c
Joel Crasta <loejstarc@gmail.com>
2024-11-04 20:09:10 +0530

    added multiple files upload to s3

--- src/lib/s3Upload.ts
+++ src/lib/s3Upload.ts
@@ -3,35 +3,41 @@ import { PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3"
 
 interface UrlResponse {
   s3Status: "success" | "error"
-  imageUrl?: string
+  fileUrls?: string[]
 }
 
-export async function imageUpload(file: File): Promise<UrlResponse> {
-  if (!file) {
-    console.error("No file provided for upload.")
+export async function fileUpload(files: File[]): Promise<UrlResponse> {
+  if (!files || files.length === 0) {
+    console.error("No files provided for upload.")
     return { s3Status: "error" }
   }
 
-  const fileName = `${Date.now()}-${file.name.replace(/\s/g, "_")}`
-  console.log("Uploading file:", fileName)
+  const fileUrls = []
 
-  const params = {
-    Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
-    Key: `icons/${fileName}`,
-    Body: file,
+  for (const file of files) {
+    const fileName = `${Date.now()}-${file.name}`
+    const params = {
+      Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
+      Key: `icons/${fileName}`,
+      Body: file,
+    }
+
+    const command = new PutObjectCommand(params)
+
+    try {
+      await s3Client.send(command)
+      fileUrls.push(
+        `https://${process.env.NEXT_PUBLIC_CLOUDFRONT_DOMAIN}/${process.env.NEXT_PUBLIC_S3_FOLDER_NAME_1}/${fileName}`
+      )
+    } catch (err) {
+      console.error("Error uploading file:", err)
+      return { s3Status: "error" }
+    }
   }
 
-  const command = new PutObjectCommand(params)
-
-  try {
-    await s3Client.send(command)
-    return {
-      s3Status: "success",
-      imageUrl: `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.amazonaws.com/icons/${fileName}`,
-    }
-  } catch (err) {
-    console.error("Error uploading file:", err)
-    return { s3Status: "error", imageUrl: "" }
+  return {
+    s3Status: "success",
+    fileUrls: fileUrls,
   }
 }
 
