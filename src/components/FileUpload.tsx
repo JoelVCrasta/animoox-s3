525bccb7e5c74e16796bbfc33e884577285062c2
Joel Crasta <loejstarc@gmail.com>
2024-11-05 01:15:05 +0530

    added file drag and fixed few issues

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,33 +1,68 @@
 "use client"
 
+import { useState } from "react"
 import { Upload } from "lucide-react"
-import toast, { Toaster } from "react-hot-toast"
 import DisplayFiles from "@/components/DisplayFiles"
 
 interface FileUploadProps {
   label: string
+  subLabel?: string
   accept: string
   files: File[] | null
   setFiles: (files: File[] | null) => void
+  height?: number
+  multiple?: boolean
 }
 
-const FileUpload = ({ label, accept, files, setFiles }: FileUploadProps) => {
-  const displayFiles = () => {
-    if (files) {
-      console.log("Files selected:", files)
+const FileUpload = ({
+  label,
+  subLabel,
+  accept,
+  files,
+  setFiles,
+  height,
+  multiple = true,
+}: FileUploadProps) => {
+  const [isDragging, setIsDragging] = useState(false)
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    setIsDragging(true)
+  }
+
+  const handleDragLeave = () => {
+    setIsDragging(false)
+  }
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    setIsDragging(false)
+
+    if (e.dataTransfer.files) {
+      setFiles(Array.from(e.dataTransfer.files))
     }
   }
 
   return (
-    <div>
+    <div className="w-full h-full">
       <label className="block text-gray-700 font-semibold mb-2">
         {label} <span className="text-red-500">*</span>
       </label>
 
-      <div className="flex flex-col items-center justify-center border-2 border-dashed border-gray-300 p-16 rounded-lg bg-gray-100 text-gray-400 font-semibold">
+      <div
+        className={`
+          flex flex-col items-center justify-center border-2 border-dashed border-gray-300  rounded-lg bg-gray-100 text-gray-400 font-semibold
+          ${height ? `h-${height}` : "p-16"}
+          ${isDragging ? "border-blue-500" : ""}
+        `}
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}
+      >
         <input
+          id={label}
           type="file"
-          multiple={true}
+          multiple={multiple}
           accept={accept}
           onChange={(e) => {
             if (e.target.files) {
@@ -35,19 +70,21 @@ const FileUpload = ({ label, accept, files, setFiles }: FileUploadProps) => {
             }
           }}
           className="hidden"
-          id="fileUpload"
         />
 
         <Upload size={60} color="#9ca3af" />
-        <label htmlFor="fileUpload" className="cursor-pointer mt-2">
+        <label htmlFor={label} className="cursor-pointer mt-2">
           Drag & Drop or{" "}
           <span className="text-blue-500 font-semibold">Choose file</span> to
           upload
         </label>
 
-        <p className="text-xs font-thin mt-2">
-          Accepted file formats: {accept}
-        </p>
+        <div className="text-center mt-2">
+          {subLabel && <p className="text-xs font-thin">{subLabel}</p>}
+          <p className="text-xs font-thin ">
+            Accepted file formats: {accept === ".zip" ? "ZIP" : "JPG, PNG"}
+          </p>
+        </div>
       </div>
 
       <DisplayFiles files={files} setFiles={setFiles} />
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,6 +2,31 @@ export interface IIconFormData {
   iconStyle: string
   license: string
   category: string
-  tags: string[] | null
+  tags: string[]
   file: string[]
+}
+
+export interface IPackFormData {
+  title: string
+  subtitle: string
+  category: string
+  description: string
+  packagePrice: number
+  discount: number
+  thumbnailUrl: string
+  illustrationUrl: string
+  animationUrl: string
+  featureImageUrl: string
+  productViewImageUrl: string[]
+  graphicFileIncluded: string
+  tags: string[]
+  keyFeatures: string[]
+}
+
+export interface IPackFiles {
+  thumbnailFile: File[] | null
+  illustrationFile: File[] | null
+  animationFile: File[] | null
+  featureImageFiles: File[] | null
+  productViewImageFiles: File[] | null
 }
