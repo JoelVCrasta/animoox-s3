1d4007550dbabfb0485578eb45df8d69c8d069b3
Joel Crasta <loejstarc@gmail.com>
2024-11-05 23:40:55 +0530

    made into a select icon field

--- src/components/CompatibilityIcons.tsx
+++ src/components/CompatibilityIcons.tsx
@@ -1,12 +1,24 @@
+import React, { useState } from "react"
 import Image from "next/image"
 
 interface IIcons {
   iconsImg: string
   alt: string
+  value: string
+  onChange: (value: string) => void
+  isSelected: boolean
 }
 
-const CompatibilityIcons = () => {
-  const iconData: IIcons[] = [
+const CompatibilityIcons = ({
+  value,
+  onChange,
+}: {
+  value: string
+  onChange: (value: string) => void
+}) => {
+  const [selectedIcon, setSelectedIcon] = useState<string | null>(null)
+
+  const iconData = [
     {
       iconsImg: "/icons/ai.png",
       alt: "Adobe Illustrator",
@@ -29,18 +41,35 @@ const CompatibilityIcons = () => {
     },
   ]
 
+  const handleIconClick = (alt: string) => {
+    setSelectedIcon(alt)
+    onChange(alt)
+  }
+
   return (
     <div className="flex gap-4">
       {iconData.map((icon, index) => (
-        <Icons key={index} iconsImg={icon.iconsImg} alt={icon.alt} />
+        <Icons
+          value={value}
+          onChange={handleIconClick}
+          key={index}
+          iconsImg={icon.iconsImg}
+          alt={icon.alt}
+          isSelected={selectedIcon === icon.alt}
+        />
       ))}
     </div>
   )
 }
 
-const Icons = ({ iconsImg, alt }: IIcons) => {
+const Icons = ({ iconsImg, alt, value, onChange, isSelected }: IIcons) => {
   return (
-    <div className="bg-background p-3 rounded-2xl">
+    <div
+      onClick={() => onChange(alt)}
+      className={`bg-background p-1 md:p-3 rounded-2xl ${
+        isSelected ? "bg-blue-400" : ""
+      }`}
+    >
       <Image src={iconsImg} alt={alt} width={30} height={30} />
     </div>
   )
