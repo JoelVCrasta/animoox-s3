99048618476994b9f0e08cc75b24336e4e233ebd
Joel Crasta <loejstarc@gmail.com>
2024-11-05 23:44:00 +0530

    added zod validation for packFormData and new schema

--- src/utils/zodSchema.ts
+++ src/utils/zodSchema.ts
@@ -8,6 +8,13 @@ export const addIconSchema = z.object({
   file: z.array(z.string()).min(1),
 })
 
+export const preAddIconSchema = z.object({
+  iconStyle: z.string().min(1),
+  license: z.string().min(1),
+  category: z.string().min(1),
+  tags: z.array(z.string()).min(1),
+})
+
 export const addPackSchema = z.object({
   title: z.string().min(1),
   subtitle: z.string().min(1),
@@ -21,6 +28,20 @@ export const addPackSchema = z.object({
   featureImageUrl: z.string().min(1),
   productViewImageUrl: z.array(z.string()).min(1),
   graphicFileIncluded: z.string().min(1),
+  compatibility: z.string().min(1),
+  tags: z.array(z.string()).min(1),
+  keyFeatures: z.array(z.string()),
+})
+
+export const preAddPackSchema = z.object({
+  title: z.string().min(1),
+  subtitle: z.string().min(1),
+  category: z.string().min(1),
+  description: z.string().min(1),
+  packagePrice: z.number().min(1),
+  discount: z.number(),
+  graphicFileIncluded: z.string().min(1),
+  compatibility: z.string().min(1),
   tags: z.array(z.string()).min(1),
   keyFeatures: z.array(z.string()),
 })
